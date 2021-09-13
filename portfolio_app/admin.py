7a0b83fd084686a875d02d823a8f252215c95168
Lotfi <lotfi.m.bendiaf@gmail.com>
2021-09-13 13:53:43 +0100

    Admin interface added

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -3,4 +3,8 @@ from .models import Message
 
 # Register your models here.
 
+admin.site.site_header = "Portfolio | Admin panel"
+admin.site.site_title = "Portfolio | Admin panel"
+admin.site.index_title = "Portfolio | Admin panel"
+
 admin.site.register(Message)
